d92c2ae68b1489788074e92341d4dac215cbf629
arthur <omiyalesamson@gmail.com>
2023-04-22 20:46:59 +0100

    second commit
    Creattion of models

--- task_manager/core/models.py
+++ task_manager/core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # Create your models here.
+
+def one_week_hence():
+    return timezone.now() + timezone.timedelta(7)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date = models.DateTimeField(null=True, blank=True)
+    due_date = models.DateTimeField(default=one_week_hence)
+    duration = models.DurationField()
+    assign_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    created_at = models.DateTimeField(auto_now_add=True)
+    task_status_choices = [
+        ('Un', 'Unassign'),
+        ('PR', 'In progress'),
+        ('Co', 'Completed'),
+    ]
+
+    task_status = models.CharField(choices=task_status_choices, default="Unassign", max_length=2)
+
+    def __str__(self):
+        return f'{self.name} created by {self.created_by} at {self.created_at}'
